cc669b62b8aa056cad797c4eee481c2f4fcca417
Pavlo <161053846+AsterPressF@users.noreply.github.com>
2024-02-24 17:17:05 +0100

    Update PlayerMove.cs

--- PlayerMovement/PlayerMove.cs
+++ PlayerMovement/PlayerMove.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField] private float speed = 5f;
+    [SerializeField] private float _speed = 5f;
 
     void Update()
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
-        transform.position += movement * speed * Time.deltaTime;
+        transform.position += movement * _speed * Time.deltaTime;
     }
 }
